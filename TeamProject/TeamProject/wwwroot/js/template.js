f6773c2f2943d47faff62b285d3a93d4dd7d94c0
Daniel Kamiński <daniel.kaminski1998@gmail.com>
2020-04-20 20:52:50 +0200

    AdminPanel part1

---
+++ TeamProject/TeamProject/wwwroot/js/template.js
@@ -0,0 +1,11 @@
+const logo = document.querySelector('.logo');
+const navbarCustom = document.querySelector('.navbar-custom');
+const customLink = document.querySelector('.custom-link');
+const linkText = document.querySelector('.link-text');
+const main = document.querySelector('main');
+let active;
+logo.addEventListener('click', () => {
+  navbarCustom.classList.toggle('navbar-custom-active');
+  main.classList.toggle('main-active');
+});
+console.log(main);
