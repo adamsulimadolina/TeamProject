429da61a7dfc89b213028e1d5782dd40e7f7000a
Adam Sulima Dolina <adam.sulimadolina@gmail.com>
2020-06-16 11:27:34 +0200

    UI fixes

--- TeamProject/TeamProject/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TeamProject/TeamProject/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -38,7 +38,7 @@ namespace TeamProject.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
+            [EmailAddress(ErrorMessage = "Proszę wprowadzić prawidłowy adres e-mail.")]
             public string Email { get; set; }
 
             [Required]
@@ -82,7 +82,7 @@ namespace TeamProject.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Użytkownik zalogowany.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -91,12 +91,12 @@ namespace TeamProject.Areas.Identity.Pages.Account
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning("Konto zablokowane");
                     return RedirectToPage("./Lockout");
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Błędny e-mail lub hasło.");
                     return Page();
                 }
             }
